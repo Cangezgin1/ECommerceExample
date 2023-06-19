f753ca5e1e4b159c6060971d0cdc33a92d1b5da8
Muhammed Can GEZGİN <93928479+Cangezgin1@users.noreply.github.com>
2023-06-19 11:32:04 +0300

    Update LoginController.cs

--- ECommerceExample/Controllers/LoginController.cs
+++ ECommerceExample/Controllers/LoginController.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ECommerceExample.Controllers
 {
-    public class LoginController : Controller
+    public class LoginController : Controller 
     {
 
         public IActionResult Index()
